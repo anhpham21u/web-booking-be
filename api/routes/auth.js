23492f01bda9360f486c7aac8d770e315268f762
anhpham <hoctap31111@gmail.com>
2023-06-04 18:43:55 +0700

    auth solve

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,9 +1,9 @@
 import express from "express";
+import { register, login } from "../controller/auth.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello fucking world!!!");
-});
+router.post("/register", register);
+router.post("/login", login);
 
 export default router;
--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,18 +1,28 @@
 import express from "express";
 import Hotel from "./../models/Hotel.js";
+import {
+  createHotel,
+  updateHotel,
+  deleteHotel,
+  getHotel,
+  getHotels,
+} from "../controller/hotel.js";
 
 const router = express.Router();
 
 // create
-router.post("/", async (req, res) => {
-  const newHotel = new Hotel(req.body);
+router.post("/", createHotel);
 
-  try {
-    const saveHotel = await newHotel.save();
-    res.status(200).json(saveHotel);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+// update
+router.put("/:id", updateHotel);
+
+// delete
+router.delete("/:id", deleteHotel);
+
+// get
+router.get("/:id", getHotel);
+
+// get all
+router.get("/", getHotels);
 
 export default router;
